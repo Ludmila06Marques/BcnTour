55191ff22948341f805464e175a7b4673fdf1d3b
Lud <instimila@gmail.com>
2022-09-30 03:35:41 +0200

    chore: add onchange on input login

--- src/Pages/Login/LoginScreen.jsx
+++ src/Pages/Login/LoginScreen.jsx
@@ -27,8 +27,8 @@ export default function LoginScreen(){
   
     <Logo/>
     <S.ContainerInputs>
-        <S.InputEmail type="text" placeholder="Email" ></S.InputEmail>
-        <S.InputPassword type="password" placeholder="Password"></S.InputPassword>
+        <S.InputEmail type="text" placeholder="Email" onChange={(e)=> setEmail(e.target.value)} value={email} ></S.InputEmail>
+        <S.InputPassword type="password" placeholder="Password" onChange={(e)=> setPassword(e.target.value)} value={password}></S.InputPassword>
         <S.LoginButton>Entrar</S.LoginButton>
         <Link to="/sign-up" >
         <S.SendToSignUp>Nao tem conta?Cadastre-se</S.SendToSignUp>
