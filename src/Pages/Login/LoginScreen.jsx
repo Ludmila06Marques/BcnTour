d2260d8b61d2d46baed01f1e1b4254a7cdf59918
Lud <instimila@gmail.com>
2022-09-30 03:33:11 +0200

    chore:add axios from login

--- src/Pages/Login/LoginScreen.jsx
+++ src/Pages/Login/LoginScreen.jsx
@@ -1,4 +1,3 @@
-import axios from "axios"
 import Logo from "../../Components/Logo/Logo.jsx"
 import * as S from "./style"
 import { Link } from "react-router-dom"
@@ -6,17 +5,31 @@ import appContext from "../../Contexts/AppContext.js"
 import { useContext } from "react"
 import { useNavigate } from "react-router-dom"
 
+
+
 export default function LoginScreen(){
- 
+       const navigate= useNavigate()
+        const {setEmail , email , setPassword , password , setName , name , setCity , city , setCountry , country , setUrlImage , urlImage}= useContext(appContext)
+
+    async function login(){
+        const body={email ,password }       
+        console.log(body)
+        try {
+            const promise= await axios.post('http://localhost:5000/login' , body)  
+            console.log(promise.data)  
+            navigate("/home")
+        } catch (error) {
+            console.log(error.response.data)
+        }
+    }
+
     return(<>
   
     <Logo/>
     <S.ContainerInputs>
-        <S.InputEmail type="text" placeholder="Email"  onChange={(e)=> setEmail(e.target.value)}  value={email}></S.InputEmail>
-        <S.InputPassword type="password" placeholder="Password" onChange={(e)=> setPassword(e.target.value)}  value={password}></S.InputPassword>
-       
-        <S.LoginButton onClick={login} >Entrar</S.LoginButton>
-     
+        <S.InputEmail type="text" placeholder="Email" ></S.InputEmail>
+        <S.InputPassword type="password" placeholder="Password"></S.InputPassword>
+        <S.LoginButton>Entrar</S.LoginButton>
         <Link to="/sign-up" >
         <S.SendToSignUp>Nao tem conta?Cadastre-se</S.SendToSignUp>
         </Link>
